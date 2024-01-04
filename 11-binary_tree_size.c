5bfb398ba4b58a360f58993bb6e305d5bb3ce912
888Greys <toxicgreys001@gmail.com>
2024-01-04 03:55:44 -0800

    Binary Trees

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+
+/**
+* binary_tree_size - binary tree size
+* @tree: binary tree
+* Return: size of tree
+* Nice example
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+size_t lSize = 0, rSize = 0;
+
+if (!tree)
+return (0);
+lSize = binary_tree_size(tree->left);
+rSize = binary_tree_size(tree->right);
+return (1 + lSize + rSize);
+}
